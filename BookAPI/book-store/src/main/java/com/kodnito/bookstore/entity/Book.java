82b7b60c1aaa117c1f4b4333eba200f3d3661c5f
Moises Lony <54813324+MoisesLony@users.noreply.github.com>
2022-10-02 19:03:53 -0600

    Parcial
    Parcial

---
+++ BookAPI/book-store/src/main/java/com/kodnito/bookstore/entity/Book.java
@@ -0,0 +1,84 @@
+package com.kodnito.bookstore.entity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+@Entity
+@Table(name = "games")
+@NamedQueries({
+        @NamedQuery(name = "Game.findAll", query = "SELECT b FROM Book b")
+})
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String game;
+    private String genre;
+    private String description;
+
+    private String developer;
+
+    private String publisher;
+    private float price;
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
